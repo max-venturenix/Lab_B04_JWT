22cb8c1b49cac2346eb46e585d06675703ff883c
max.venturenix <max.venturenix@gmail.com>
2023-03-01 16:48:05 +0800

    Updated using jwtDecoder bean

--- src/main/java/com/fsse/lab_b04_jwt/security/SecurityConfig.java
+++ src/main/java/com/fsse/lab_b04_jwt/security/SecurityConfig.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -15,21 +17,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
+                .authorizeHttpRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .csrf().disable();
-
-        // Set and validate the issuer
-        http
-                .oauth2ResourceServer(
-                        oauth2ResourceServer -> oauth2ResourceServer.jwt(
-                                jwt -> jwt.decoder(
-                                        JwtDecoders.fromIssuerLocation(issuer))
-                        )
-                );
         return http.build();
+    }
+
+
+    // Decode the JWT and validate the issuer
+    @Bean
+    JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuer);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+        return jwtDecoder;
     }
 }
 
